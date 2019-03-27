e77ff1c04052cd7a41358c6a5ba37b7d0d30361f
bianca-camargo-machado <bianca.camargo.machado@hp.com>
2019-03-27 18:50:04 -0300

    [componente] botão de next

--- src/screens/Inicio.js
+++ src/screens/Inicio.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';  
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, StyleSheet} from 'react-native';
+import BotaoProximo from '../components/botaoProximo';
 
 export default class Home extends Component {  
   render() {
     return (
-      <View>
-        <Text>Inicio</Text>
-        
-        <Button
-          title="Criar Sala"
-          color="green"
-          onPress={() => this.props.navigation.navigate('Sala')}
+      <View style={styles.container}>
+        <View>
+          <Text>Inicio</Text>
+
+          <Button
+            title="Criar Sala"
+            color="green"
+            onPress={() => this.props.navigation.navigate('Sala')}
+          />
+        </View>
+
+        <BotaoProximo 
+          endereco='Convidados' 
+          navigation={this.props.navigation} 
         />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  }
+});
