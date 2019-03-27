4ca7b114b87c11fe283e6640270e241005789212
JM Fantin <joao.fantin@acad.pucrs.br>
2019-03-27 18:18:45 -0300

    primeiro model

--- src/screens/Sala.js
+++ src/screens/Sala.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';  
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, Alert } from 'react-native';
+import Sala from '../Models/Sala.model';
+
+import { db } from '../config';
+
+let submeterSala = sala => {  
+  db.ref('/salas').push(sala);
+};
 
 export default class Home extends Component {  
+  
+  sala = new Sala(
+    'JM',
+    'Opcoes do churrasco',
+    'Isso tem que virar descrição e pdf',
+    'Isso tem que virar as questões',
+    'Isso tem que virar os convidados'
+  )
+
+  handleSubmit = () => {
+    submeterSala(this.sala);
+    Alert.alert('Sala salva.');
+  };
+
   render() {
     return (
       <View>
@@ -11,6 +32,11 @@ export default class Home extends Component {
           title="Voltar tela de Inicio"
           color="blue"
           onPress={() => this.props.navigation.navigate('Inicio')}
+        />        
+        <Button
+          title="Testar SalaFactory!"
+          color="red"
+          onPress={() => this.handleSubmit()}
         />
         <Button
           title="Criar questões"
