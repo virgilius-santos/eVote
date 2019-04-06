aca4ef41f0b753dcfca79e7b82a9cbdb0c5cb5da
bianca-camargo-machado <bianca.camargo.machado@hp.com>
2019-04-06 01:21:37 -0300

    ajuste de pesquisa sem email ou cpf

--- src/screens/Convidados.js
+++ src/screens/Convidados.js
@@ -15,21 +15,21 @@ export default class Convidados extends Component {
     super(props);
     this.state = {
       convidados: [
-        { nome: "Amanda Santos", cpf: '04194123456', incluido: true },
-        { nome: "Brenda Silva", cpf: '03457824903', incluido: true },
-        { nome: "Bruno Almeida", cpf: '28405784953', incluido: true },
-        { nome: "Cássia Lucas", cpf: '34573568543', incluido: false },
-        { nome: "Cássio Andrade", cpf: '21357568765', incluido: false },
-        { nome: "Ana Oliveira", cpf: '34723679876', incluido: false },
-        { nome: "João Silva", cpf: '85687456837', incluido: false },
-        { nome: "Cássia Lucas", cpf: '789145365789', incluido: false },
-        { nome: "Cássio Andrade", cpf: '14578365147', incluido: false },
-        { nome: "Ana Oliveira", cpf: '02145878963', incluido: false },
-        { nome: "João Silva", cpf: '14785298741', incluido: false },
-        { nome: "Cássia Lucas", cpf: '00011133344', incluido: false },
-        { nome: "Cássio Andrade", cpf: '01478936514', incluido: false },
-        { nome: "Ana Oliveira", cpf: '78912315947', incluido: false },
-        { nome: "João Silva", cpf: '96345874122', incluido: false },
+        { nome: "Amanda Santos", cpf: '04194123456', incluido: false },
+        { nome: "Brenda Silva", cpf: '03457824903', incluido: false, email: 'brenda@gmail.com' },
+        { nome: "Bruno Almeida", cpf: '28405784953', incluido: false, email: 'bruno@gmail.com' },
+        { nome: "Cássia Lucas", cpf: '34573568543', incluido: false, email: 'cassia@gmail.com' },
+        { nome: "Cássio Andrade", cpf: '21357568765', incluido: false, email: 'amanda@gmail.com' },
+        { nome: "Ana Oliveira", cpf: '34723679876', incluido: false, email: 'ana@gmail.com' },
+        { nome: "João Silva", cpf: '85687456837', incluido: false, email: 'joao@gmail.com' },
+        { nome: "Cássia Lucas", cpf: '789145365789', incluido: false, email: 'cassio@gmail.com' },
+        { nome: "Cássio Andrade", cpf: '14578365147', incluido: false, email: 'cassio1@gmail.com' },
+        { nome: "Ana Oliveira", cpf: '02145878963', incluido: false, email: 'ana1@gmail.com' },
+        { nome: "João Silva", cpf: '14785298741', incluido: false, email: 'joao1@gmail.com' },
+        { nome: "Cássia Lucas", cpf: '00011133344', incluido: false, email: 'cassia2@gmail.com' },
+        { nome: "Cássio Andrade", cpf: '01478936514', incluido: false, email: 'cassio2@gmail.com'  },
+        { nome: "Ana Oliveira", cpf: '78912315947', incluido: false, email: 'ana2@gmail.com' },
+        { nome: "João Silva", cpf: '96345874122', incluido: false, email: 'joao2@gmail.com' },
       ],
       pesquisa: null
     }
@@ -41,14 +41,27 @@ export default class Convidados extends Component {
   };
 
   handleSearch = (value) => {
+    let items;
     if(value) {
       const encontrados = this.state.convidados.filter(item => {
-        const items = 
-        `${item.nome.toUpperCase()}
-          ${item.cpf.toUpperCase()}
-          ${item.email.toUpperCase()}
-          ${item.incluido}`;
-        
+        if(item.cpf && item.email) {
+          items = 
+          `${item.nome.toUpperCase()}
+            ${item.cpf.toUpperCase()}
+            ${item.email.toUpperCase()}
+            ${item.incluido}`;
+        } else if(item.cpf) {
+          items = 
+          `${item.nome.toUpperCase()}
+            ${item.cpf.toUpperCase()}
+            ${item.incluido}`;
+        } else if(item.email) {
+          items = 
+          `${item.nome.toUpperCase()}
+            ${item.email.toUpperCase()}
+            ${item.incluido}`;
+        }
+
         const text = value.toUpperCase();
           
         return items.indexOf(text) > -1;    
@@ -68,7 +81,8 @@ export default class Convidados extends Component {
     if(pesquisa) {
 
       convidadosAtualizados.map(item => {
-        if(item.cpf == pesquisa[index].cpf || item.email == pesquisa[index].email) {
+        if(item.cpf == pesquisa[index].cpf 
+          || item.email == pesquisa[index].email) {
           item.incluido 
             ? item.incluido = false
             : item.incluido = true;
