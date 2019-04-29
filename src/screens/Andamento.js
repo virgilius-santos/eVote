d7e0493602f16e307abf9261c87325dd7911f062
bianca-camargo-machado <bianca.camargo.machado@hp.com>
2019-04-29 17:14:31 -0300

    merge

--- src/screens/Andamento.js
+++ src/screens/Andamento.js
@@ -1,48 +1,41 @@
-import React, {Component} from 'react'; 
-import {Text,View,StyleSheet} from 'react-native';
-import BotaoAnterior from '../components/BotaoAnterior';//componentes criados
-import BotaoProximo from '../components/BotaoProximo'; //componentes criados
+import React, { Component } from 'react'; 
+import { Text, View } from 'react-native';
+import BotaoAnterior from '../components/BotaoAnterior';
+import BotaoProximo from '../components/BotaoProximo';
 import styles from '../styles/estilos';
 import StatusVotacao from '../components/StatusVotacao';
 
 export default class Andamento extends Component {
-    constructor(props) {
-        super(props);
-    }
-    static navigationOptions = {
-        title: 'Sala: Cores pro App',
-      };
-      handleSubmit=()=>{
+  constructor(props) {
+      super(props);
+  }
+  static navigationOptions = {
+      title: 'Sala: Cores pro App',
+  };
+  
+  handleSubmit=()=>{}
 
-      }
-      render() {
-
-        return (
-        <View style={styles.container}>
+  render() {
+    return (
+      <View style={styles.container}>
         <Text>
             Essa sala foi criada para decidirmos o layout do aplicativo pooler da Ages 2019/1, o E-Vote. Agente precsa de um texto longo pa oi sajsk
         </Text>
-        <View style = {styles.statusStyle}>
-        <StatusVotacao tipo = 'outro' texto = "33% dos usuários já votaram"/>
-        <StatusVotacao tipo = 'hora' texto = "A votação encerra as 06h do dia 13/03"/>
+        <View>
+          <StatusVotacao tipo = 'usuario' texto = "33% dos usuários já votaram"/>
+          <StatusVotacao tipo = 'hora' texto = "A votação encerra as 06h do dia 13/03"/>
         </View>
         <View style={styles.flowButtonsContainer}>
-        <BotaoAnterior 
-          endereco='Inicio' 
-          navigation={this.props.navigation} 
-        />
-        <BotaoProximo
-          endereco='SalaContexto'
-          onPress={() => this.handleSubmit()}
-        />
+          <BotaoAnterior 
+            endereco='Convidados' 
+            navigation={this.props.navigation} 
+          />
+          <BotaoProximo
+            endereco='Inicio'
+            onPress={() => this.handleSubmit()}
+          />
+        </View>
       </View>
-      </View>
-            );
-        }
-}
-const statusStyle = StyleSheet.create({
-  status: {
-    
-    alignItems: "center"
+    );
   }
-});
+}
