e2bea01ce934d15737e4e6aed8202c484245afd9
MathiasGattiElbern <gaia@1994>
2019-05-26 20:46:56 -0300

    Handling user login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';  
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, Alert, Button, StyleSheet, StatusBar } from 'react-native';
 import styles from '../styles/estilos';
 import LoginForm from '../components/LoginForm';
 
@@ -8,9 +8,20 @@ export default class Login extends Component {
     constructor(props) {
         super(props) 
         this.state = {
-          salas: {}
+          salas: {},
+          email_cpf: '',
+          senha: ''
         }      
     }
+
+    handleLogin = () => {
+      const { email, pasword } = this.state
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(() => this.props.navigation.navigate('Inicio'))
+        .catch(error => this.setState({ errorMessage: error.message }))
+        }
 
     static navigationOptions = {
         title: 'Login',
@@ -24,7 +35,31 @@ export default class Login extends Component {
          </View>
 
           <View style={styles.formContainer}>
-            <LoginForm />
+          <TextInput style = {styles.loginInput} 
+               autoCapitalize="none" 
+               onSubmitEditing={() => this.passwordInput.focus()} 
+               autoCorrect={false} 
+               keyboardType='email-address' 
+               returnKeyType="next"
+               onChangeText={email => this.setState({ email })}
+               value={this.state.email} 
+               placeholder='Email ou CPF' 
+               placeholderTextColor='rgba(225,225,225,0.7)'/>
+
+            <TextInput style = {styles.loginInput}   
+              returnKeyType="go" 
+              ref={(input)=> this.passwordInput = input} 
+              placeholder='Senha'
+              onChangeText={password => this.setState({ password })}
+              value={this.state.password} 
+              placeholderTextColor='rgba(225,225,225,0.7)' 
+              secureTextEntry/>
+
+                <TouchableOpacity style={styles.loginButtonContainer} 
+                     onPress={onButtonPress}>
+                    <Text  style={styles.loginButtonText}>LOGIN</Text>
+                </TouchableOpacity> 
+            
           </View>
         </View>
         );  
