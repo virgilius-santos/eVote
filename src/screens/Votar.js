88d6dd45e5230961f03630b1cd133c13d949ac66
Daniela Amaral <danielarosa2309@gmail.com>
2019-05-28 19:18:17 -0300

    renomeando

--- src/screens/Votar.js
+++ src/screens/Votar.js
@@ -1,42 +1,121 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import BotaoProximo from '../components/BotaoProximo';
+import BotaoAnterior from '../components/BotaoAnterior';
+import { db } from '../config';
+import { ActivityIndicator, View, Text } from 'react-native';
 import styles from '../styles/estilos';
+import Progresso from '../components/Progresso';
+import BotaoMedio from '../components/BotaoMedio';
 
 export default class Votar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            titulo: '',
-            alternativas: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+      size: 5
     }
-    static navigationOptions = ({ navigation }) => ({
-        title: `Pergunta: ${navigation.state.params.numero}`,
-    });
+  }
 
-    componentWillMount() {
-        const titulo = this.props.navigation.getParam('titulo', null);
+  static navigationOptions = ({ navigation }) => ({
+    // title: `Sala: ${navigation.state.params.sala.titulo}`,
+    title: 'Sala',
+    headerLeft: null
+  });
+
+  handleSubmit = async () => {
+    const sent = await this.sendData();
+    if (sent) {
+      this.setState({ sending: false });
+      this.setState({ sent: true });
     }
-    render() {
-        return (
-            <View style={styles.container}>
-                <ScrollView style={[{ alignSelf: 'auto' }, { marginTop: 10 }]}>
-                    <FlatList
-                        style={{ marginTop: 20 }}
-                        data={alternativas}
-                        numColumns={1}
-                        renderItem={({ item, index }) => (
-                            <BotaoAlternativa
-                                onPress={() => this.handleSelect(index)}
-                                index={index}
-                                text={item}
-                                selected={selected}>
-                            </BotaoAlternativa>
-                        )}
-                        keyExtractor={(item, index) => index.toString()}
-                    />
-                </ScrollView></View>
-      </View >
-    );
+    this.setState({ sending: false });
+  }
+
+  sendData = async () => {
+    this.setState({ sending: true });
+    const response_sala = await
+      //TODO chamar os dados das alternativas
+      db.ref('salas/').push({
+        ...sala
+      }).then(() => {
+        return true;
+      }).catch((error) => {
+        console.warn('error ', error);
+        return false;
+      });
+  }
+
+  handleNavigation = (mudanca) => {
+    const { index } = this.state;
+    if (mudanca === 0) {
+      this.setState({ index: index - 1 })
+    } else {
+      this.setState({ index: index + 1 })
     }
+  }
+
+  render() {
+    const { sending, sent, index } = this.state;
+    return (
+      sent ?
+        <View>
+          <Text style={{
+            alignSelf: 'center',
+            color: '#8400C5',
+            fontSize: 20,
+            fontWeight: 'bold'
+          }}>
+            Sala cadastrada com sucesso!
+        </Text>
+
+        </View> :
+        sending ?
+          <View>
+            <Text style={{
+              alignSelf: 'center',
+              color: '#8400C5',
+              fontSize: 20,
+              fontWeight: 'bold'
+            }}>
+              Salvando a sala...
+        </Text>
+            <ActivityIndicator
+              animating={sending}
+              size="large"
+              color="#00DC7B"
+            />
+          </View> :
+          //{index < tamanho de questoes ? botoes abaixo : <BotaoMedio texto="Continuar" onPress={() => this.props.navigation.navigate('Inicio')} />}
+          <View style={[styles.flowButtonsContainer, { alignSelf: "auto" }, { marginTop: 5 }]}>
+            <Text>{index}</Text>
+            <Text>{this.state.index.valueOf() < this.state.size.valueOf()}</Text>
+
+
+            <BotaoAnterior
+              endereco='QuestaoSalva'
+              onPress={() => this.handleNavigation(0)}
+            />
+            {this.state.index.valueOf() < this.state.size.valueOf() ?
+              <View>
+                <Progresso quantidade={5} total={5} />
+                <BotaoProximo
+                  endereco='Inicio'
+                  onPress={() => this.handleNavigation(1)}
+                />
+              </View>
+
+              :
+
+              <BotaoMedio
+                texto="Continuar"
+                onPress={() => this.props.navigation.navigate('Inicio')}
+              />
+            }
+
+          </View>
+
+    )
+
+
+  }
 }
