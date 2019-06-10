d9be9bc6694f2d1a8515efce489d4a4c23a45da1
Carolina Schmitt Fração <carolina.fracao@edu.pucrs.br>
2019-06-10 19:27:13 -0300

    Coloquei para voltar para a tela de login e o botão de confirmar

--- src/screens/TelaCadastro.js
+++ src/screens/TelaCadastro.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { View, Image, TextInput, TouchableOpacity,Text, StyleSheet } from 'react-native';
+import { View, Image, TextInput, TouchableOpacity,Text, StyleSheet, KeyboardAvoidingView } from 'react-native';
 import InputTexto from '../components/InputTexto';
 import styles from '../styles/estilos';
 import DateInput from '../components/DateInput';
 import InputEmail from '../components/InputEmail';
 import InputSenha from '../components/InputSenha';
+import BotaoAnterior from '../components/BotaoAnterior';
+import BotaoGrande from '../components/BotaoGrande';
 
 export default class TelaCadastro extends Component{
     constructor(props) {
@@ -20,13 +22,21 @@ export default class TelaCadastro extends Component{
         title: 'Registrar',
     };
 
+    handleCadastro = () => {
+        const { email, senha } = this.state}
+
     render(){
         return(
-           <View style={styles.container}>
-               <View style={{flex: 1}}>
-                    <InputTexto
+        <KeyboardAvoidingView behavior={"padding"} style={styles.container} enabled number="2" >   
+            <View style={{flex: 1} [styles.flowButtonsContainer, { marginTop: 5 }]}>
+                <BotaoAnterior 
+                    
+                    endereco='Login' 
+                     navigation={this.props.navigation} 
+                />
+                <InputTexto
                      label= "Nome"/>
-                    <InputEmail 
+                <InputEmail 
                     autoCorrect={false} 
                     keyboardType='email-address'
                     returnKeyType="next"
@@ -35,19 +45,27 @@ export default class TelaCadastro extends Component{
                     placeholder='E-mail' />
                     <InputTexto
                     label= "CPF"/>
-               </View>
-               <View style={{flex: 2, backgroundColor: 'white'}}>
+            </View>
+                <View style={{flex: 2, backgroundColor: 'white'} }>
                     <DateInput
-                    titulo="Data de nascimento"/>
+                        titulo="Data de nascimento"
+                    />
                     <InputSenha
-                    autoCorrect={false} 
-                    returnKeyType="go" 
-                    ref={(input)=> this.passwordInput = input} 
-                    placeholder='Senha'
-                    onChangeText={senha => this.setState({ senha })}
-                    value={this.state.senha}/>
-                </View>
-           </View> 
+                        autoCorrect={false} 
+                        returnKeyType="go" 
+                        ref={(input)=> this.passwordInput = input} 
+                        placeholder='Senha'
+                        onChangeText={senha => this.setState({ senha })}
+                        value={this.state.senha}
+                    />
+                    <BotaoGrande
+                        texto="Confirmar"
+                        onPress={() => this.handleCadastro()}
+                        endereco='Login' 
+                        navigation={this.props.navigation} 
+                    />
+            </View>
+    </KeyboardAvoidingView>      
         )
     }
 }
