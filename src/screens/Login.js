64b2c0f53efe6be2595d8ce6651ab9e37631e4ca
Vinicius Branco <vinicius.branco@acad.pucrs.br>
2019-11-04 11:09:20 -0300

    input cpf

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -5,6 +5,8 @@ import InputEmail from '../components/InputEmail';
 import InputSenha from '../components/InputSenha';
 import styles from '../styles/estilos';
 import * as Google from 'expo-google-app-auth';
+import Aviso from '../components/Aviso';
+import InputTexto from '../components/InputTexto';
 
 export default class Login extends Component {
   constructor(props) {
@@ -14,6 +16,8 @@ export default class Login extends Component {
       email: 'email@id.com',
       senha: '123456',
       errorMessage: '',
+      cpf: '',
+      errorCPF: '',
       signedIn: false,
       name: "",
       photoUrl: ""
@@ -36,7 +40,7 @@ export default class Login extends Component {
   }
 
   static navigationOptions = {
-    title: 'Bem-vind@ ao eVote!'
+    title: 'Bem-vindo ao eVote!'
   };
 
   static getUID = () => {
@@ -51,11 +55,12 @@ export default class Login extends Component {
       })
 
       if (result.type === "success") {
-        console.log("RESULTADO",result)
+        console.log("RESULTADO", result)
         this.setState({
           signedIn: true,
           name: result.user.name,
-          photoUrl: result.user.photoUrl
+          photoUrl: result.user.photoUrl,
+          email:result.user.email
         })
       } else {
         console.log("cancelled")
@@ -64,7 +69,7 @@ export default class Login extends Component {
       console.log("error", e)
     }
   }
-  
+
   render() {
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding" enabled number="2">
@@ -72,28 +77,44 @@ export default class Login extends Component {
           <Image resizeMode="contain" style={styles.logo} source={require("../../assets/icon.png")} />
         </View>
         <View style={{ flex: 3 }}>
-          <InputEmail
-            autoCorrect={false}
-            keyboardType='email-address'
-            returnKeyType="next"
-            label={'login'}
-            onChangeText={email => this.setState({ email })}
-            value={this.state.email}
-            placeholder='E-mail' />
+          {
+            this.state.signedIn ? (
+              <View>
+                <Text>Insira seu CPF para finalizar cadastro</Text>
+                <Aviso texto={this.state.errorMessage} />
+                <InputTexto
+                  label="CPF"
+                  value={this.state.cpf}
+                  onChangeText={cpf => this.setState({ cpf, errorCPF: '', errorMessage: '' })} />
+              </View>
+            ) : (
+                <View>
+                  <View style={styles.loginContainer}>
+                    <Image resizeMode="contain" style={styles.logo} source={require("../../assets/icon.png")} />
+                  </View>
 
-          <InputSenha
-            autoCorrect={false}
-            returnKeyType="go"
-            placeholder='Senha'
-            label={'senha'}
-            onChangeText={senha => this.setState({ senha })}
-            value={this.state.senha} />
+                  <InputEmail
+                    autoCorrect={false}
+                    keyboardType='email-address'
+                    returnKeyType="next"
+                    label={'login'}
+                    onChangeText={email => this.setState({ email })}
+                    value={this.state.email}
+                    placeholder='E-mail' />
 
-          <Text style={custom.notice}>
-            {this.state.errorMessage}
-          </Text>
+                  <InputSenha
+                    autoCorrect={false}
+                    returnKeyType="go"
+                    placeholder='Senha'
+                    label={'senha'}
+                    onChangeText={senha => this.setState({ senha })}
+                    value={this.state.senha} />
 
-
+                  <Text style={custom.notice}>
+                    {this.state.errorMessage}
+                  </Text>
+                </View>
+              )}
         </View>
         <View style={{ flex: 3, backgroundColor: 'white' }}>
           <TouchableOpacity style={styles.loginButtonContainer}
@@ -106,11 +127,8 @@ export default class Login extends Component {
             <Text style={styles.loginButtonText}>Cadastrar</Text>
           </TouchableOpacity>
 
-          {this.state.signedIn ? (
-            <LoggedInPage name={this.state.name} photoUrl={this.state.photoUrl} />
-          ) : (
-              <LoginPage signIn={this.signIn} />
-            )}
+          <LoginPage signIn={this.signIn} />
+
         </View>
 
       </KeyboardAvoidingView>
@@ -128,7 +146,7 @@ const custom = StyleSheet.create({
 const LoginPage = props => {
   return (
     <View>
-      <Button title="Sign in with Google" onPress={() => props.signIn()} />
+      <Button style={{ marginTop: 15 }} title="Sign in with Google" onPress={() => props.signIn()} />
     </View>
   )
 }
@@ -161,3 +179,43 @@ const googleStyle = StyleSheet.create({
     borderRadius: 150
   }
 })
+
+isCPFValido = async () => {
+  let Soma;
+  let Resto;
+  let validated = true;
+  Soma = 0;
+  if (this.state.cpf.length != 11) {
+    return this.setState({ errorCPF: 'CPF deve ter 11 numeros' });
+  }
+
+  if (this.state.cpf == "00000000000") validated = false;
+
+  for (i = 1; i <= 9; i++) Soma = Soma + parseInt(this.state.cpf.substring(i - 1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+  if ((Resto == 10) || (Resto == 11)) Resto = 0;
+  if (Resto != parseInt(this.state.cpf.substring(9, 10))) validated = false;
+
+  Soma = 0;
+  for (i = 1; i <= 10; i++) Soma = Soma + parseInt(this.state.cpf.substring(i - 1, i)) * (12 - i);
+  Resto = (Soma * 10) % 11;
+
+  if ((Resto == 10) || (Resto == 11)) Resto = 0;
+  if (Resto != parseInt(this.state.cpf.substring(10, 11))) validated = false;
+
+  if (validated == false) this.setState({ errorCPF: 'CPF inválido' });
+
+  const usersCount = await new Promise(resolve => {
+    db.ref('usuarios').orderByChild('cpf').equalTo(this.state.cpf).on('value', (snapshot) => {
+      resolve(snapshot.numChildren());
+    });
+  });
+
+  if (usersCount > 0) {
+    validated = false;
+    this.setState({ errorCPF: 'CPF já está em uso' });
+  }
+
+  return validated;
+}
